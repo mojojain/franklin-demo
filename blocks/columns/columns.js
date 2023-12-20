d2e6f4e188214035567efa5948f2998f567b452f
Manoj Jain <manosezen@gmail.com>
2023-12-20 14:24:27 +0530

    Updated columns

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,3 +1,5 @@
+import { buildBlock, loadBlock } from "../../scripts/lib-franklin.js";
+
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
@@ -15,4 +17,38 @@ export default function decorate(block) {
       }
     });
   });
+
+  if (block.closest('.about-us')) {
+    // Build social-links blocks
+    const socialLinks = block.querySelectorAll('div div p:last-child');
+    socialLinks.forEach((socialLink) => {
+      const allSocialLinks = socialLink.querySelectorAll('a');
+      const paramArr = [];
+      allSocialLinks.forEach(link => {
+        link.classList.add('button');
+        paramArr.push(link.outerHTML);
+      }); 
+      
+      const socialLinksBlock = buildBlock('social-links', paramArr.join(''));
+      socialLink.innerHTML = socialLinksBlock.outerHTML;
+      socialLink.dataset.blockName = 'social-links';
+      loadBlock(socialLink);
+    });
+
+    // Build bio-card blocks
+    const allContents = block.children[0].children;
+    [... allContents].forEach((content) => {
+      const paramArr = [];
+      paramArr.push(`<div>${content.querySelector('picture').outerHTML}</div>`);
+      paramArr.push(`<div>${content.querySelector('p:nth-child(2)').innerHTML}</div>`);
+      paramArr.push(`<div>${content.querySelector('p:nth-child(3)').innerHTML}</div>`);
+      const bioCard = buildBlock('bio-card', paramArr.join(''));
+      bioCard.querySelector('div>div>div').classList.add('bio-card');
+      bioCard.dataset.blockName = 'bio-card';
+      const socialLinks = content.querySelector('.social-links');
+      content.innerHTML = bioCard.outerHTML;
+      content.appendChild(socialLinks);
+      loadBlock(content.querySelector('.bio-card'));
+    });
+  }
 }
